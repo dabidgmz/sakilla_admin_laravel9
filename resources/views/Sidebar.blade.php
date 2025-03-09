1425e17e5e2d961a08bf6b16c9375be4fd400e0d
David Herrera <david.gmzherrera28@gmail.com>
2025-03-09 04:56:07 -0600

    se agrego scroll en el sidebar

--- resources/views/Sidebar.blade.php
+++ resources/views/Sidebar.blade.php
@@ -22,139 +22,141 @@
 
     <!-- Sidebar Menu -->
     <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-item menu-open">
-                <a href="#" class="nav-link active" style="background-color: #ffd700 !important; color: #000000 !important; border-radius: 5px;">
-                    <i class="nav-icon fas fa-film" style="color: #000000 !important;"></i>
-                    <p>
-                        Cinema Studio System
-                        <i class="right fas fa-angle-left" style="color: #000000 !important;"></i>
-                    </p>
-                </a>
-                <ul class="nav nav-treeview">
-                    <!-- Actors -->
-                    <li class="nav-item">
-                        <a href="{{ route('Actors') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Actors</p>
-                        </a>
-                    </li>
+        <div style="max-height: 400px; overflow-y: auto;">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active" style="background-color: #ffd700 !important; color: #000000 !important; border-radius: 5px;">
+                        <i class="nav-icon fas fa-film" style="color: #000000 !important;"></i>
+                        <p>
+                            Cinema Studio System
+                            <i class="right fas fa-angle-left" style="color: #000000 !important;"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <!-- Actors -->
+                        <li class="nav-item">
+                            <a href="{{ route('Actors') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Actors</p>
+                            </a>
+                        </li>
 
-                    <!-- Address -->
-                    <li class="nav-item">
-                        <a href="{{ route('Address') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Address</p>
-                        </a>
-                    </li>
+                        <!-- Address -->
+                        <li class="nav-item">
+                            <a href="{{ route('Address') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Address</p>
+                            </a>
+                        </li>
 
-                    <!-- Categories -->
-                    <li class="nav-item">
-                        <a href="{{ route('Categories') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Categories</p>
-                        </a>
-                    </li>
+                        <!-- Categories -->
+                        <li class="nav-item">
+                            <a href="{{ route('Categories') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Categories</p>
+                            </a>
+                        </li>
 
-                    <!-- Citys -->
-                    <li class="nav-item">
-                        <a href="{{ route('Citys') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Citys</p>
-                        </a>
-                    </li>
+                        <!-- Citys -->
+                        <li class="nav-item">
+                            <a href="{{ route('Citys') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Citys</p>
+                            </a>
+                        </li>
 
-                    <!-- Customers -->
-                    <li class="nav-item">
-                        <a href="{{ route('Customers') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Customers</p>
-                        </a>
-                    </li>
+                        <!-- Customers -->
+                        <li class="nav-item">
+                            <a href="{{ route('Customers') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Customers</p>
+                            </a>
+                        </li>
 
-                    <!-- Films -->
-                    <li class="nav-item">
-                        <a href="{{ route('Flims') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Flims</p>
-                        </a>
-                    </li>
+                        <!-- Films -->
+                        <li class="nav-item">
+                            <a href="{{ route('Flims') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Flims</p>
+                            </a>
+                        </li>
 
-                    <!-- Film Actors -->
-                    <li class="nav-item">
-                        <a href="{{ route('Flim_Actor') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Flim Actors</p>
-                        </a>
-                    </li>
+                        <!-- Film Actors -->
+                        <li class="nav-item">
+                            <a href="{{ route('Flim_Actor') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Flim Actors</p>
+                            </a>
+                        </li>
 
-                    <!-- Film Categories -->
-                    <li class="nav-item">
-                        <a href="{{ route('Flim_Category') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Flimm Categories</p>
-                        </a>
-                    </li>
+                        <!-- Film Categories -->
+                        <li class="nav-item">
+                            <a href="{{ route('Flim_Category') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Flimm Categories</p>
+                            </a>
+                        </li>
 
-                    <!-- Film Text -->
-                    <li class="nav-item">
-                        <a href="{{ route('Flim_text') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Flimm Text</p>
-                        </a>
-                    </li>
+                        <!-- Film Text -->
+                        <li class="nav-item">
+                            <a href="{{ route('Flim_text') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Flimm Text</p>
+                            </a>
+                        </li>
 
-                    <!-- Inventory -->
-                    <li class="nav-item">
-                        <a href="{{ route('Inventory') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Inventory</p>
-                        </a>
-                    </li>
+                        <!-- Inventory -->
+                        <li class="nav-item">
+                            <a href="{{ route('Inventory') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Inventory</p>
+                            </a>
+                        </li>
 
-                    <!-- Languages -->
-                    <li class="nav-item">
-                        <a href="{{ route('Languages') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Languages</p>
-                        </a>
-                    </li>
+                        <!-- Languages -->
+                        <li class="nav-item">
+                            <a href="{{ route('Languages') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Languages</p>
+                            </a>
+                        </li>
 
-                    <!-- Payments -->
-                    <li class="nav-item">
-                        <a href="{{ route('Payments') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Payments</p>
-                        </a>
-                    </li>
+                        <!-- Payments -->
+                        <li class="nav-item">
+                            <a href="{{ route('Payments') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Payments</p>
+                            </a>
+                        </li>
 
-                    <!-- Rentals -->
-                    <li class="nav-item">
-                        <a href="{{ route('Rentals') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Rentals</p>
-                        </a>
-                    </li>
+                        <!-- Rentals -->
+                        <li class="nav-item">
+                            <a href="{{ route('Rentals') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Rentals</p>
+                            </a>
+                        </li>
 
-                    <!-- Staff -->
-                    <li class="nav-item">
-                        <a href="{{ route('Staff') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Staff</p>
-                        </a>
-                    </li>
+                        <!-- Staff -->
+                        <li class="nav-item">
+                            <a href="{{ route('Staff') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Staff</p>
+                            </a>
+                        </li>
 
-                    <!-- Stores -->
-                    <li class="nav-item">
-                        <a href="{{ route('Stores') }}" class="nav-link">
-                            <i class="nav-icon far fa-circle text-danger"></i>
-                            <p>Stores</p>
-                        </a>
-                    </li>
+                        <!-- Stores -->
+                        <li class="nav-item">
+                            <a href="{{ route('Stores') }}" class="nav-link">
+                                <i class="nav-icon far fa-circle text-danger"></i>
+                                <p>Stores</p>
+                            </a>
+                        </li>
 
-                </ul>
-            </li>
-        </ul>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </nav>
     <!-- /.sidebar-menu -->
 </div>
